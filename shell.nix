796d2abcfcf0d9256940da08cd4d5fe91510d6e9
Corban R <corban@raunco.co>
2021-12-05 21:36:30 +0000

    Update crystal version to 1.2.2

--- shell.nix
+++ shell.nix
@@ -1,43 +1,7 @@
-{ pkgs ? import <nixpkgs> {}, ...}:
-
-with pkgs;
-
-let
-  inherit (darwin.apple_sdk.frameworks) CoreServices ApplicationServices Security;
-  darwin_packages = [CoreServices ApplicationServices Security];
-
-  # Python virtualenv
-  pythonPackages = python3Packages;
-  venvDir = "./.venv";
-
-in mkShell rec {
-  name = "ansible-crystal";
-
-  buildInputs = [
-    bash-completion
-    git
-    pythonPackages.python
-  ] ++ stdenv.lib.optional stdenv.isDarwin darwin_packages;
-
-  shellHook = ''
-    # For python virtualenv
-    SOURCE_DATE_EPOCH=$(date +%s)
-    if [ -d "${venvDir}" ]; then
-      echo "Skipping venv creation, '${venvDir}' already exists"
-    else
-      echo "Creating new venv environment in path: '${venvDir}'"
-      ${pythonPackages.python.interpreter} -m venv "${venvDir}"
-    fi
-    PYTHONPATH=$PWD/${venvDir}/${pythonPackages.python.sitePackages}/:$PYTHONPATH
-    source "${venvDir}/bin/activate"
-
-    # Add additional folders to to XDG_DATA_DIRS if they exists, which will get sourced by bash-completion
-    for p in ''${buildInputs}; do
-      if [ -d "$p/share/bash-completion" ]; then
-        XDG_DATA_DIRS="$XDG_DATA_DIRS:$p/share"
-      fi
-    done
-
-    source ${bash-completion}/etc/profile.d/bash_completion.sh
-  '';
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
+    sha256 = "sha256:0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5"; }
+) {
+  src =  ./.;
+}).shellNix.default
